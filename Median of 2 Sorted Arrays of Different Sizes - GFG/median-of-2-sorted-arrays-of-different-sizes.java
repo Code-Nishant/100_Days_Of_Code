b0bb8f74421d92483d2b75f2bde780cab1acdb3b
Nishant Choudhary <87940978+Code-Nishant@users.noreply.github.com>
2022-10-10 23:28:52 +0530

    Added solution - LeetHub

--- Median of 2 Sorted Arrays of Different Sizes - GFG/median-of-2-sorted-arrays-of-different-sizes.java
+++ Median of 2 Sorted Arrays of Different Sizes - GFG/median-of-2-sorted-arrays-of-different-sizes.java
@@ -49,7 +49,7 @@ class GFG
  
         while (low <= high) {
             int mid1 = (low + high) / 2;
-            int mid2 = (n + m + 1) / 2 - mid1;
+            int mid2 = (len1 + len2 + 1) / 2 - mid1;
             
             int l1= (mid1 > 0) ? nums1[mid1 - 1]: Integer.MIN_VALUE;
             int l2= (mid2 > 0) ? nums2[mid2 - 1]: Integer.MIN_VALUE;
@@ -71,41 +71,5 @@ class GFG
                 low = mid1 + 1;
         }
         return 0.0;
-        
-        
-        // int len1=nums1.length;
-        // int len2=nums2.length;
-        
-        // if(len1>len2){
-        //     return medianOfArrays(len1,len2,nums2,nums1);
-        // }
-        
-        // int low=0;
-        // int high=len1;
-        // while(low<=high){
-        //     int mid1=(low+high)/2;
-        //     int mid2=((len2+len1+1)/2)-mid1;
-            
-        //     int l1=(mid1<0)?Integer.MIN_VALUE:nums1[mid1-1];
-        //     int l2=(mid2<0)?Integer.MIN_VALUE:nums2[mid2-1];
-        //     int r1=(mid1>len1)?Integer.MAX_VALUE:nums1[mid1];
-        //     int r2=(mid2>len2)?Integer.MAX_VALUE:nums2[mid2];
-            
-        //     if(l1>r2){
-        //         high= mid1-1;
-        //     }
-        //     else if(l2>r1){
-        //         low=mid1+1;
-        //     }
-        //     else{
-        //         if((len1+len2)/2==0){
-        //             return ((Math.min(l1,l2)+Math.max(r1,r2))/2);
-        //         }
-        //         else{
-        //             Math.max(l1,l2);
-        //         }
-        //     }
-        // }
-        // return 0.0;
     }
 }
